9bb604291770548e7e199fde2cc21b0bd22be376
Leander (Macbook) <me@leander.ca>
2016-05-18 03:15:37 -0700

    Stub out all the tests

--- test/basic.js
+++ test/basic.js
@@ -23,6 +23,10 @@ describe('API Test', function () {
       assert.ok(isStream.readable(controller))
     })
 
+    it('should set options', function () {
+      // Check setting multiple times
+    })
+
   })
   
   describe('log', function () {
@@ -35,19 +39,88 @@ describe('API Test', function () {
     it('should be readable', function () {
       var log = BetterLogs('section');
       assert.ok(isStream.readable(log))
+      // Hook and test
+    })
+
+    it('should override console', function () {
     })
 
   })
 
   describe('defaults', function () {
 
-    it('should have basic log functions', function () {
+    it('should have basic log types', function () {
     })
 
     it('should have basic modes', function () {
     })
     
     it('should have stdout output', function () {
+    })
+
+  })
+
+  describe('custom log types', function () {
+
+    it('should format basic', function () {
+      // Try all formats
+    })
+    
+    it('should format stack', function () {
+    })
+    
+    it('should format function', function () {
+    })
+    
+  })
+
+  describe('custom output', function () {
+
+    it('should set default output', function () {
+    })
+
+    it('should set section output', function () {
+    })
+
+    it('should set section/type output', function () {
+    })
+
+  })
+
+  describe('show and hide', function () {
+
+    it('should show/hide by default', function () {
+    })
+
+    it('should show/hide section', function () {
+    })
+
+    it('should show/hide section/type', function () {
+    })
+
+    it('should show/hide groups', function () {
+    })
+
+    it('should show/hide groups within groups', function () {
+    })
+
+  })
+
+  describe('modes', function () {
+
+    it('should show/hide by default', function () {
+    })
+
+    it('should show/hide section', function () {
+    })
+
+    it('should show/hide section/type', function () {
+    })
+
+    it('should show/hide groups', function () {
+    })
+
+    it('should show/hide groups within groups', function () {
     })
 
   })
